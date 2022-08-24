96b2dd60cb69fbbb8a2ba711d7c860bf7965a82e
0rque <shchuk1naaams@gmail.com>
2022-08-24 14:30:04 +0400

    Get Post by ID REST API implemented;

--- src/main/java/com/springboot/blog/service/PostService.java
+++ src/main/java/com/springboot/blog/service/PostService.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface PostService {
 
     PostDto createPost(PostDto postDto);
-
     List<PostDto> getAllPosts();
+
+    PostDto getPostById(long id);
 }
--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -1,6 +1,7 @@
 package com.springboot.blog.service.impl;
 
 import com.springboot.blog.entity.Post;
+import com.springboot.blog.exception.ResourceNotFoundException;
 import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.PostService;
@@ -33,6 +34,12 @@ public class PostServiceImpl implements PostService {
     public List<PostDto> getAllPosts() {
         List<Post> posts = postRepository.findAll();
         return posts.stream().map(this::mapToDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        return mapToDto(post);
     }
 
     //convert Entity into DTO
